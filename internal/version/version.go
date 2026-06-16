37f65ddad24e04d765fba115a29724e453bb2437
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2026-06-16 19:18:43 -0300

    feat(profile): pprof helpers + make profile-mem/cpu (P131)

--- internal/version/version.go
+++ internal/version/version.go
@@ -4,7 +4,7 @@ package version
 
 var (
 	// Set via -ldflags "-X github.com/ropeixoto/harnessx/internal/version.Version=..."
-	Version = "0.97.0"
+	Version = "0.98.0"
 	Commit  = "unknown"
 	Date    = "unknown"
 )
