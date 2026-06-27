ddb500f0141f8e00f6b6d73b1b65f664bbf4d7b1
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2026-06-27 13:13:08 -0300

    chore(release): bump to v0.159.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -4,7 +4,7 @@ package version
 
 var (
 	// Set via -ldflags "-X github.com/ropeixoto/harnessx/internal/version.Version=..."
-	Version = "v0.158.0"
+	Version = "v0.159.0"
 	Commit  = "unknown"
 	Date    = "unknown"
 )
