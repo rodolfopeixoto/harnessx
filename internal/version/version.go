af843ef07324d1e65f9ba296e51ae2cf9f161e4e
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2026-06-28 10:15:35 -0300

    chore(release): bump to v0.168.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,10 +1,7 @@
-// SPDX-License-Identifier: MIT
-
 package version
 
 var (
-	// Set via -ldflags "-X github.com/ropeixoto/harnessx/internal/version.Version=..."
-	Version = "v0.167.0"
+	Version = "v0.168.0"
 	Commit  = "unknown"
 	Date    = "unknown"
 )
